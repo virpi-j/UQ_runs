a989c426eb1594e29b74c3f40d45ce5d3ff9f890
Junttila Virpi <Virpi.Junttila@env.fi>
2021-10-29 14:56:52 +0300

    Update

--- UQ_run.R
+++ UQ_run.R
@@ -12,7 +12,7 @@ UQanalysis <- "True"
 #devtools::source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/finRuns/Rsrc/sampleRun.r")
 ##### From GitHub
 devtools::source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/finRuns/Rsrc/settings.r")
-source_url("https://raw.githubusercontent.com/virpi-j/UQ_runs/main/functions.R")
+source_url("https://raw.githubusercontent.com/virpi-j/UQ_runs/main/UQ_functions.R")
 #source_url("https://raw.githubusercontent.com/ForModLabUHel/IBCcarbon_runs/master/general/functions.r")
 #source("Rsrc/virpiSbatch/functionsUQ.r")
 source("Rsrc/virpiSbatch/localSettings.r")
