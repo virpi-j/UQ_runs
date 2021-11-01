96d5cc682a82f6c7f7d3dd5788b671f003411303
Junttila Virpi <Virpi.Junttila@env.fi>
2021-11-01 16:24:33 +0200

    Update

--- UQ_run.R
+++ UQ_run.R
@@ -58,27 +58,60 @@ if(uncRun){
 #  timeRun <- endRun - startRun
 #  print(paste0("Run time for sample size ", nSitesRun," = ",timeRun))
 #} else {
+rcpfile = rcps
+# for(rcpfile in rcps) { ## ---------------------------------------------
+#  print(rcpfile)
+if(rcpfile=="CurrClim"){
+  load(paste(climatepath, rcpfile,".rdata", sep=""))  
+  #####process data considering only current climate###
+  # dat <- dat[rday %in% 1:10958] #uncomment to select some years (10958 needs to be modified)
+  maxRday <- max(dat$rday)
+  xday <- c(dat$rday,(dat$rday+maxRday),(dat$rday+maxRday*2))
+  dat = rbind(dat,dat,dat)
+  dat[,rday:=xday]
+  
+} else{
+  load(paste(climatepath, rcpfile,".rdata", sep=""))  
+}
+##
+niter <- ceil(nSamples/10/(10000/nSitesRun))
+
+sampleOutput <- list()
+
+for(nii in 1:niter){
   toMem <- ls()
-  print("Start running...")
+  print("Start running iter ",nii,"/",niter,"...")
   startRun <- Sys.time() 
   #sampleXs <- lapply(sampleIDs[1:10], function(jx) {
   #  runModelUQ(jx,  ## Do nothing for 10 seconds
   #  uncRun = TRUE)})      
-  sampleXs <- mclapply(sampleIDs, function(jx) {
+  sampleXs <- mclapply(sampleIDs[((nii-1)*5):(nii*5)], function(jx) {
     runModelUQ(jx,  ## Do nothing for 10 seconds
     uncRun = uncRun)}, 
     mc.cores = nCores,mc.silent=FALSE)      ## Split this job across 10 cores
   timeRun <- Sys.time() - startRun
   print(paste0("Run time for ",nSamples," samples of size ", nSitesRun," = ",timeRun))
-  #}
-#}
-print("End running...")
-  
-save(sampleXs,file=paste0("Rsrc/virpiSbatch/results/samplex_",r_no,".rdata")) 
-setwd("Rsrc/virpiSbatch/")
+  print("End running...")
 
-#source("postprocessResults.R")
+  save(sampleXs,file=paste0("Rsrc/virpiSbatch/results/samplex_",r_no,".rdata")) 
+  setwd("Rsrc/virpiSbatch/")
 
+  m <- nrow(sampleXs[[1]])
+  n <- length(sampleXs)
+  varNams <-  sampleXs[[1]][,"vari"]
+  cS <- c(-44/12/10^12, 16^2, 16^2, 0.16^2)
+  # g /m2 /year -> -44/12*16^2/10^12
+  for(j in 1:m){
+    x <- data.frame()
+    for(k in 1:n){
+      x <- rbind(x, sampleXs[[k]][j,])
+    }
+    x[,3:5] <- x[,3:5]*cS[j]
+    #assign(varNams[j,1], x)
+    sampleOutputx[[j]][((nii-1)*5):(nii*5),] <- x
+  }
+}
+  #source("postprocessResults.R")
 
 print("make histograms...")
 pdf(paste0("/scratch/project_2000994/PREBASruns/finRuns/Rsrc/virpiSbatch/figures/results_regionID_",r_no,".pdf"))
--- functions.R
+++ functions.R
@@ -37,23 +37,24 @@ runModelUQ <- function(sampleID,sampleRun=FALSE,ststDeadW=FALSE,
   i = 0
   # load("/scratch/project_2000994/PREBASruns/metadata/initSoilCstst.rdata")
   # load("outSoil/InitSoilCstst_Base.rdata")
-  rcpfile = rcps
-  # for(rcpfile in rcps) { ## ---------------------------------------------
-  # print(rcpfile)
-  if(rcpfile=="CurrClim"){
-    load(paste(climatepath, rcpfile,".rdata", sep=""))  
-    #####process data considering only current climate###
-    # dat <- dat[rday %in% 1:10958] #uncomment to select some years (10958 needs to be modified)
-    maxRday <- max(dat$rday)
-    xday <- c(dat$rday,(dat$rday+maxRday),(dat$rday+maxRday*2))
-    dat = rbind(dat,dat,dat)
-    dat[,rday:=xday]
+  if(!uncRun){
+    rcpfile = rcps
+    # for(rcpfile in rcps) { ## ---------------------------------------------
+    #  print(rcpfile)
+    if(rcpfile=="CurrClim"){
+      load(paste(climatepath, rcpfile,".rdata", sep=""))  
+      #####process data considering only current climate###
+      # dat <- dat[rday %in% 1:10958] #uncomment to select some years (10958 needs to be modified)
+      maxRday <- max(dat$rday)
+      xday <- c(dat$rday,(dat$rday+maxRday),(dat$rday+maxRday*2))
+      dat = rbind(dat,dat,dat)
+      dat[,rday:=xday]
     
-  } else{
-    load(paste(climatepath, rcpfile,".rdata", sep=""))  
-  }
-  # load("C:/Users/minunno/Documents/research/lukeDB/example #2/CanESM2.rcp45.rdata")
-  
+    } else{
+      load(paste(climatepath, rcpfile,".rdata", sep=""))  
+    }
+    # load("C:/Users/minunno/Documents/research/lukeDB/example #2/CanESM2.rcp45.rdata")
+  }  
   ## Loop regions -------------------------------------------------------
   # for (r_no in regions) {
   # print(date())
